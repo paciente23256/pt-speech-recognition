ceea790db13dfcf0083927442195f698f16bdcb7
paciente23256 <93782123+paciente23256@users.noreply.github.com>
2023-01-22 01:22:56 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# MESI-TB 2023 - Aluno # 23256
+# Web App com Flask e Audio 2 text converter
+
+#Bibibliotecas
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
+
+# Iniciar App com Flask com os metodos get e post
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+
+# Funcao que cria o formulario (importa wav e transcreve para texo com  speechrecognizer
+def pagina():
+    transcript = ""
+    if request.method == "POST":
+        print("DADOS RECEBIDOS - FICHEIRO WAV ")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, language='pt-PT') # pode ser alterado manualmente para a lingua pretendia por ex. es-ES
+
+    return render_template('app.html', transcript=transcript) # importa o ficheiro template app.html
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
